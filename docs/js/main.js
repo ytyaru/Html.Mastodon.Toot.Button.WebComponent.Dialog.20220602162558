cddec30c73053e952d3ff028dc1feadfe6c54fb5
ytyaru <yttry0@gmail.com>
2022-06-02 17:39:35 +0900

    バグ修正（ボタンを押すたびlocation.hrefが増える）

--- docs/js/main.js
+++ docs/js/main.js
@@ -3,6 +3,7 @@ window.addEventListener('DOMContentLoaded', async (event) => {
     document.getElementById('toot-button').addEventListener('click', (event) => {
         document.getElementById('toot-dialog').showModal();
         const status = document.getElementById('status');
+        if ('init' in status.dataset) { status.value = status.dataset.init }
         status.value += '\n' + location.href
         status.focus();
         status.setSelectionRange(0, 0);
