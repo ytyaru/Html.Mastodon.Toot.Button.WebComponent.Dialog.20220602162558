b9a820d25ff356e5aa1577c4d6e5683700bd7d8e
ytyaru <yttry0@gmail.com>
2022-06-02 21:59:10 +0900

    不要コメント削除

--- docs/js/main.js
+++ docs/js/main.js
@@ -6,29 +6,18 @@ window.addEventListener('DOMContentLoaded', async (event) => {
         if ('init' in status.dataset) { status.innerHTML = status.dataset.init }
         status.innerHTML += '<br>' + location.href
         status.focus();
-        //status.setSelectionRange(0, 0);
         setCaretStart(status)
         document.getElementById('status').dispatchEvent(new Event('input'))
-        /*
-        document.getElementById('toot-dialog').showModal();
-        const status = document.getElementById('status');
-        if ('init' in status.dataset) { status.value = status.dataset.init }
-        status.value += '\n' + location.href
-        status.focus();
-        status.setSelectionRange(0, 0);
-        document.getElementById('status').dispatchEvent(new Event('input'))
-        */
     });
     document.getElementById('status').addEventListener('input', (event) => {
         const LIMIT = 500
-        //console.log(event.target.value)
-        //const remaining = LIMIT - event.target.value.length
         const remaining = LIMIT - event.target.innerText.length
         console.log(remaining )
         document.getElementById('status-remaining').textContent = remaining;
         console.log(event.target.innerText)
     });
     function setCaretStart(target) {
+        //status.setSelectionRange(0, 0);
         var range = document.createRange()
         var sel = window.getSelection()
         range.setStart(target.childNodes[0], 0)
